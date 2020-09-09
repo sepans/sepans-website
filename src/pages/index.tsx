4fb7d20e777c060907e5676c14d8b6813d8679ec
sepans <sepans@sepans.com>
2020-09-09 10:47:17 -0400

    links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,51 @@ const IndexPage: React.FC = () => (
   <Layout>
     <SEO title="Home" />
     <Title>Sepand Ansari</Title>
-    
-    </Layout>
+    <Links>
+      <Link target="_blank" href="https://purplebulldozer.com">
+        purple bulldozer
+      </Link>
+      <Link
+        target="_blank"
+        href="https://www.goodreads.com/user/show/22717-sepand"
+      >
+        goodreads
+      </Link>
+      <Link target="_blank" href="https://photos.app.goo.gl/5zZy6MCcMiavUdLx9">
+        random projects
+      </Link>
+      <Link target="_blank" href="https://github.com/sepans/">
+        github
+      </Link>
+      <Link target="_blank" href="https://www.linkedin.com/in/sepans/">
+        linkedin
+      </Link>
+      <Link target="_blank" href="https://www.strava.com/athletes/14784454">
+        strava
+      </Link>
+    </Links>
+  </Layout>
 )
 
-const Title = styled.h1 `
-  &::first-letter {
-    font-style:italic;
-    font-weight:400;
+const Links = styled.div`
+  font-family: Inter;
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+`
+
+const Link = styled.a`
+  color: #333;
+  &:visited {
+    color: #353535;
   }
+  &:hover {
+    color: #000;
+  }
+`
+
+const Title = styled.h1 `
   font-family: Inter;
   font-size:48px;
   color:black;
