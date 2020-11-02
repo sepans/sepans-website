8bb890163a95d0e571e72991faa0072189f29526
sepans <sepans@sepans.com>
2020-11-02 09:04:35 -0500

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,12 @@ module.exports = {
         fonts: [`Inter:400,700`],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-11178641-1",
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
