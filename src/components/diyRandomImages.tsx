4c1ab64ba3a299a18cab9a5bc342d67d6dd70c93
sepans <sepans@sepans.com>
2025-01-01 04:35:54 -0500

    test use state

--- src/components/diyRandomImages.tsx
+++ src/components/diyRandomImages.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { random } from 'lodash'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useFlickrImages } from '../hooks/useFlickrImages'
 import { Link, PreviewContainer } from '../pages'
@@ -9,8 +9,11 @@ import { Link, PreviewContainer } from '../pages'
 export const DiyRandomImages: React.FC = () => {
   const isSSR = typeof window === 'undefined'
 
+  const [isClientRendered, setIsClientRendered] = useState(false)
+
   useEffect(() => {
     console.log('client rendering', isSSR)
+    setIsClientRendered(true)
   }, [])
 
   const { photoGroups } = useFlickrImages()
@@ -32,7 +35,15 @@ export const DiyRandomImages: React.FC = () => {
             {group.slice(randomIndex, randomIndex + 1).map((photo) => (
               <Link href="content/diy">
                 {/* don't bother rendering images when it is server side */}
-                <img height={150} src={photo.thumbnailSrc} alt={photo.label} />
+                {isClientRendered ? (
+                  <img
+                    height={150}
+                    src={photo.thumbnailSrc}
+                    alt={photo.label}
+                  />
+                ) : (
+                  <div>blah</div>
+                )}
               </Link>
             ))}
           </PreviewContainer>
