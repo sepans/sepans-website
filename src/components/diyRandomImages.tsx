aca90f15d3bfe0e84261b258928a22bbc3da4259
Sepand Ansari <sepans@sepans.com>
2025-01-01 04:51:51 -0500

    Test client side render (#24)
    * check if component is rendered on client
    
    * cleanup
    
    * better placeholder when ssr?
    
    * test use effect
    
    * test use state
    
    * cleanup state
    
    * render with low opacity on server side

--- src/components/diyRandomImages.tsx
+++ src/components/diyRandomImages.tsx
@@ -1,10 +1,20 @@
+'use client'
+
 import { random } from 'lodash'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useFlickrImages } from '../hooks/useFlickrImages'
 import { Link, PreviewContainer } from '../pages'
 
 export const DiyRandomImages: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isClientRendering, setIsClientRendering] = useState(false)
+
+  useEffect(() => {
+    // re-render images when rendered on client side to randomize
+    setIsClientRendering(true)
+  }, [])
+
   const { photoGroups } = useFlickrImages()
 
   const groupsSize = Object.keys(photoGroups).length
@@ -23,7 +33,12 @@ export const DiyRandomImages: React.FC = () => {
           <PreviewContainer key={`group-${label}`} title={label}>
             {group.slice(randomIndex, randomIndex + 1).map((photo) => (
               <Link href="content/diy">
-                <img src={photo.thumbnailSrc} alt={photo.label} />
+                <img
+                  style={{ opacity: isClientRendering ? 1 : 0.2 }}
+                  height={150}
+                  src={photo.thumbnailSrc}
+                  alt={photo.label}
+                />
               </Link>
             ))}
           </PreviewContainer>
