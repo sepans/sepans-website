4b8b5601404dcb217f4268d40eb6afc3926fa78c
sepans <sepans@sepans.com>
2025-01-01 04:17:30 -0500

    better placeholder when ssr?

--- src/components/diyRandomImages.tsx
+++ src/components/diyRandomImages.tsx
@@ -9,10 +9,6 @@ import { Link, PreviewContainer } from '../pages'
 export const DiyRandomImages: React.FC = () => {
   const isSSR = typeof window === 'undefined'
 
-  // don't bother rendering anything on the server-side. let client side hydration render images
-  if (isSSR) {
-    return <div />
-  }
   const { photoGroups } = useFlickrImages()
 
   const groupsSize = Object.keys(photoGroups).length
@@ -31,7 +27,12 @@ export const DiyRandomImages: React.FC = () => {
           <PreviewContainer key={`group-${label}`} title={label}>
             {group.slice(randomIndex, randomIndex + 1).map((photo) => (
               <Link href="content/diy">
-                <img src={photo.thumbnailSrc} alt={photo.label} />
+                {/* don't bother rendering images when it is server side */}
+                <img
+                  height={150}
+                  src={isSSR ? photo.thumbnailSrc : ''}
+                  alt={photo.label}
+                />
               </Link>
             ))}
           </PreviewContainer>
