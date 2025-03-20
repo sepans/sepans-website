18cbdec37eee888333f3f6139ce1226eb0d440f0
Sepand Ansari <sepans@sepans.com>
2025-03-20 20:01:26 +0330

    fix vazirman font, load it properly from assets (#31)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,5 @@
 import React from 'react'
 
-const Header: React.FC<{ includeFonts?: true }> = ({ includeFonts }) => (
-  <head>
-    {includeFonts && (
-      <>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap"
-          rel="stylesheet"
-        />
-      </>
-    )}
-  </head>
-)
+const Header: React.FC = () => <header />
 
 export default Header
